6d27edb16a157bf6a64b171abfa5238756c90cd0
molinje <molinje@gmail.com>
2025-04-22 21:22:04 +0000

    update Commit on development

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("logaligroup.invoices.controller.MainView", {
@@ -14,6 +16,30 @@ sap.ui.define([
         },
         onFilter: function(oEvent){
 
+            let filters = [];
+
+            const oData = this.getView().getModel("selectionScreen").getData();
+
+            if (oData.ShipName !== ""){
+                
+                // Con filters.push se adiciona un registro al array filters, el registro es de tipo Filter
+                filters.push(new Filter("ShipName", FilterOperator.Contains, oData.ShipName))
+
+            }
+
+            if (oData.CountryKey !== ""){
+                
+                // Con filters.push se adiciona un registro al array filters, el registro es de tipo Filter
+                filters.push(new Filter("Country", FilterOperator.EQ, oData.CountryKey))
+
+            }
+             
+            // invoicesList es el id del Listado de la vista, con la siguiente linea obtenemos una instancia del listado 
+            // que se esta mostrando en la vista 
+            const oList = this.getView().byId("invoicesList");
+            const oBinding = oList.getBinding("items");
+            oBinding.filter(filters);
+
         },
         onFilterClear: function(oEvent){
 
@@ -21,6 +47,11 @@ sap.ui.define([
 
            oModelSelScreen.setProperty("/ShipName", "");
            oModelSelScreen.setProperty("/CountryKey", "");
+           // para limpiar los filtros le enviamos un array vacio para que me consulte de nuevo 
+           // el listado por medio del servicio
+           const oList = this.getView().byId("invoicesList");
+           const oBinding = oList.getBinding("items");
+           oBinding.filter([]);
           
 
 
