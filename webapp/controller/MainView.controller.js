22f0735b880244616f68b95fa412cf71a447c572
molinje <molinje@gmail.com>
2025-04-22 09:08:44 +0000

    update Commit on development

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -5,6 +5,26 @@ sap.ui.define([
 
     return Controller.extend("logaligroup.invoices.controller.MainView", {
         onInit() {
-        }
+            const oJSONModel = new sap.ui.model.json.JSONModel();
+            const oView = this.getView();
+            oJSONModel.loadData("./model/SelectionScreenMenu.json");
+            oView.setModel(oJSONModel, "selectionScreen" );
+
+
+        },
+        onFilter: function(oEvent){
+
+        },
+        onFilterClear: function(oEvent){
+
+           const oModelSelScreen =  this.getView().getModel("selectionScreen");
+
+           oModelSelScreen.setProperty("/ShipName", "");
+           oModelSelScreen.setProperty("/CountryKey", "");
+          
+
+
+        },
+
     });
 });
